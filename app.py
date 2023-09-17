11340e9393b33197f1f58a297e2fc02558a28d9b
wmonarchw <ilyazaw@mail.ru>
2023-09-18 02:39:52 +0700

    redirect

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def start():
+    return redirect('/menu', code=302)
+
+@app.route('/menu')
+def menu():
     return """
 <!DOCTYPE html>
 <html lang="ru">
@@ -22,6 +26,7 @@ def start():
 </body>
 </html>
 """
+
 @app.route('/lab1')
 def lab1():
     return """
