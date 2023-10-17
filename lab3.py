1f17875b8d4de34241300123e4ed537ebf11045f
wmonarchw <ilyazaw@mail.ru>
2023-10-18 00:18:41 +0700

    8 задание

--- lab3.py
+++ lab3.py
@@ -19,3 +19,30 @@ def form1():
         errors['user'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else: 
+        price = 70
+     
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price +=10
+
+    return render_template('pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('success.html')
