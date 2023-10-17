80f3c0392bc212b7637e2492dd09ef69654a4d25
wmonarchw <ilyazaw@mail.ru>
2023-10-17 23:50:47 +0700

    валидация

--- lab3.py
+++ lab3.py
@@ -4,11 +4,18 @@ lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('lab3.html', user=user)
+    return render_template('lab3.html')
 
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
     age = request.args.get('age')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+        user = ''
+        errors['user'] = 'Заполните поле!'
     sex = request.args.get('sex')
-    return render_template('form1.html', user=user, age=age, sex=sex)
+    return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
