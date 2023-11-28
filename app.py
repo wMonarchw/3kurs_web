7668328c963a78873ff629921c4e713b9e342c1d
wMonarchw <ilyazaw@mail.ru>
2023-11-28 19:57:41 +0700

    zagatovka 6 lab

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 app.secret_key = '123'
@@ -14,4 +15,5 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
---
+++ lab6.py
@@ -0,0 +1,5 @@
+from flask import Blueprint, redirect, url_for, render_template, Blueprint, request, session
+
+
+
+lab6 = Blueprint('lab6', __name__)
