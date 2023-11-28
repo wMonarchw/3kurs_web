902b9a601873ba280c44b2d9a4b01f876f848753
wMonarchw <ilyazaw@mail.ru>
2023-11-28 21:00:58 +0700

    zakid

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, Blueprint, request
+from Db import db
+from Db.models import users
+from flask_login import LoginManager
+
 from lab1 import lab1 
 from lab2 import lab2
 from lab3 import lab3
@@ -8,6 +12,29 @@ from lab6 import lab6
 
 app = Flask(__name__)
 app.secret_key = '123'
+user_db = 'admin_knowledge_base_orm'
+host_ip = '127.0.0.1'
+host_port = '5432'
+database_name = 'knowledge_base_orm'
+password = '123'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+# Flask-login
+
+login_manager = LoginManager()
+
+login_manager.login_view = 'lab6.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(user_id):
+    return users.query.get(int(user_id))
+
+
+
 
 
 app.register_blueprint(lab1)
--- lab6.py
+++ lab6.py
@@ -1,5 +1,62 @@
 from flask import Blueprint, redirect, url_for, render_template, Blueprint, request, session
+from Db import db
 
-
+from Db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user
 
 lab6 = Blueprint('lab6', __name__)
+
+@lab6.route('/lab6/check')
+def main():
+    my_users = users.query.all()
+    print(my_users)
+    return 'console'
+
+@lab6.route('/lab6/checkarticles')
+def checkarticles():
+    my_articles = articles.query.all()
+    print(my_articles)
+    return 'console'
+
+
+@lab6.route('/lab6/register', methods = ['GET', 'POST'])
+def register():
+    errors = []
+    if request.method == 'GET':
+        return render_template('register.html')
+    
+    username_form = request.form.get('username')
+    password_form = request.form.get('password')
+
+    isUserExist = users.query.filter_by(username=username_form).first()
+    if isUserExist is not None:
+        return render_template('register.html')
+    
+    if not (username_form or password_form):
+        errors.append("Пожалуйста заполните все поля")
+        print(errors)
+        return render_template('register.html', errors=errors)
+    if username_form == '':
+        errors.append("Пожалуйста заполните все поля")
+        print(errors)
+        return render_template('register.html', errors=errors)
+    if password_form == '':
+        errors.append("Пожалуйста заполните все поля")
+        print(errors)
+        return render_template('register.html', errors=errors)
+    
+
+    hashPassword = generate_password_hash(password_form, method='pbkdf2')
+    newUser = users(username=username_form, password=hashPassword)
+
+    db.session.add(newUser)
+    db.session.commit()
+
+    return redirect('/lab6/login')
+
+
+
+
+
+
