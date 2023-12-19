9e44a2bb984217896dc0c1065699d480ea5d0dad
wMonarchw <ilyazaw@mail.ru>
2023-12-19 23:41:34 +0700

    удаление курса

--- lab8.py
+++ lab8.py
@@ -7,3 +7,23 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def lab():
     return render_template('lab8/index.html')
+
+courses = [
+    {"name": "C++", "videos": 3, "price": 3000},
+    {"name": "basic", "videos": 30, "price": 0},
+    {"name": "C#", "videos": 8}
+
+]
+
+@lab8.route('/lab8/api/courses', methods=['GET'])
+def get_courses():
+    return courses
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    return courses[course_num]
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['DELETE'])
+def del_course(course_num):
+    del courses[course_num]
+    return '', 204
